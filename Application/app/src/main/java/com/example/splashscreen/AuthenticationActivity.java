335d1042f4daf0bca1fe829196ce468ccf48c1a5
Tinotenda Mhedziso <tinomhedziso22@gmail.com>
2025-09-01 16:03:00 +0200

    Updated project
    1. Added SplashScreen
    2. Added Design for Auth (Login | SignUp)
    3. Backend still needs to be processed
    4. No transitions between screens yet

---
+++ Application/app/src/main/java/com/example/splashscreen/AuthenticationActivity.java
@@ -0,0 +1,33 @@
+package com.example.splashscreen;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AuthenticationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_authentication);
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment_container, new LoginFragment())
+                .commit();
+    }
+
+    public void showSignupFragment() {
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment_container, new SignupFragment())
+                .addToBackStack(null) // Allows the user to press the back button
+                .commit();
+    }
+
+    public void showLoginFragment() {
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment_container, new LoginFragment())
+                .addToBackStack(null)
+                .commit();
+    }
+}
